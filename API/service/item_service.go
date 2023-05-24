6b9ef92f6c7779f16b8431420f3887d80fbb5cd2
Jeraldyn <jeraldynelena@gmail.com>
2023-05-23 19:24:56 -0500

    Actualizando API

--- API/service/item_service.go
+++ API/service/item_service.go
@@ -6,30 +6,37 @@ type ItemService struct {
 	DbHandler model.DBHandler
 }
 
+// GetItems implements the function GetItems of the database in a DBHandler.
 func (s *ItemService) GetItems() ([]model.Item, error) {
 	return s.DbHandler.GetItems()
 }
 
+// GetItemsPerPage implements the function GetItemsPerPage of the database in a DBHandler.
 func (s *ItemService) GetItemsPerPage(pages, itemsPerPage int) ([]model.Item, int, error) {
 	return s.DbHandler.GetItemsPerPage(pages, itemsPerPage)
 }
 
+// GetItemId implements the function GetItemId of the database in a DBHandler.
 func (s *ItemService) GetItemId(id int) (model.Item, error) {
 	return s.DbHandler.GetItemId(id)
 }
 
+// GetItemName implements the function GetItemName of the database in a DBHandler.
 func (s *ItemService) GetItemName(name string) ([]model.Item, error) {
 	return s.DbHandler.GetItemName(name)
 }
 
+// CreateItem implements the function CreateItem of the database in a DBHandler.
 func (s *ItemService) CreateItem(newItem model.Item) (model.Item, error) {
 	return s.DbHandler.CreateItem(newItem)
 }
 
+// UpdateItem implements the function UpdateItem of the database in a DBHandler.
 func (s *ItemService) UpdateItem(id int, item model.Item) (model.Item, error) {
 	return s.DbHandler.UpdateItem(id, item)
 }
 
+// DeleteItem implements the function DeleteItem of the database in a DBHandler.
 func (s *ItemService) DeleteItem(id int) error {
 	return s.DbHandler.DeleteItem(id)
 }
